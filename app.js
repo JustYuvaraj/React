db5ef4bd57310233d573356a1ea9632335e576e0
JustYuvaraj <yuvarajnothing@gmail.com>
2025-05-04 13:32:04 +0530

    react components

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = (
-    <h1 className='head' tabIndex="5"> I am just Yuvaraj!  </h1> 
+const Title = () => (
+  <div id="title" className="head" tabIndex="5">
+    <h1 className="head" tabIndex="5">GoodBye From just Yuvaraj!</h1>
+    <h2 className="head" tabIndex="5">Founder of CodeFromScratch</h2>
+  </div>
 );
+
+const Heading = () => (
+  <div id="heading" className="head" tabIndex="5">
+    <h1 className="head" tabIndex="5">Will see You Soon !!!</h1>
+    <Title />
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<Heading />);
