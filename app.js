c7e9385a0b3cbf1ab30c15b5041894106edc56d8
JustYuvaraj <yuvarajnothing@gmail.com>
2025-05-04 13:02:20 +0530

    learnt Jsx

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-    const parent = React.createElement('div', {
-      id: 'parent'
-    }, [
-      React.createElement('div', { id: 'child1' }, [
-        React.createElement('h1', {}, "I am h1 tag"),
-        React.createElement('h1', {}, "I am h1 tag")
-      ]),
-      React.createElement('div', { id: 'child2' }, [
-        React.createElement('h1', {}, "I am h1 tag"),
-        React.createElement('h1', {}, "I am h1 tag")
-      ]),
-    ]);
-  
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(parent);
-  
+const heading = (
+    <h1 className='head' tabIndex="5"> I am just Yuvaraj!  </h1> 
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
